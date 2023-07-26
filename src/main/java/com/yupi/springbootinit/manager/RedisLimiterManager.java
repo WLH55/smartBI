c11e4f917d094a93982ac90259348fc9b0d2b26e
WLH <2109236440@qq.com>
2023-07-26 20:04:04 +0800

    引入redisson限流

---
+++ src/main/java/com/yupi/springbootinit/manager/RedisLimiterManager.java
@@ -0,0 +1,42 @@
+package com.yupi.springbootinit.manager;
+
+import com.yupi.springbootinit.common.ErrorCode;
+import com.yupi.springbootinit.exception.ThrowUtils;
+import org.apache.lucene.store.RateLimiter;
+import org.redisson.api.RRateLimiter;
+import org.redisson.api.RateIntervalUnit;
+import org.redisson.api.RateType;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * @author WLH
+ * @verstion 1.0
+ */
+@Service
+public class RedisLimiterManager {
+
+    @Resource
+    private RedissonClient redissonClient;
+    /**
+     * 限流
+     * @param key
+     */
+
+    public void doRateLimiter(String key){
+        //创建一个限流器
+        RRateLimiter rateLimiter = redissonClient.getRateLimiter(key);
+        // 每秒最多访问 2 次
+        // 参数1 type：限流类型，可以是自定义的任何类型，用于区分不同的限流策略。
+        // 参数2 rate：限流速率，即单位时间内允许通过的请求数量。
+        // 参数3 rateInterval：限流时间间隔，即限流速率的计算周期长度。
+        // 参数4 unit：限流时间间隔单位，可以是秒、毫秒等。
+        rateLimiter.trySetRate(RateType.OVERALL, 2, 1, RateIntervalUnit.SECONDS);
+        // 每当一个操作来了后，请求一个令牌
+        boolean canOp = rateLimiter.tryAcquire(1);
+        ThrowUtils.throwIf(!canOp, ErrorCode.TOO_MANY_REQUEST);
+
+    }
+}
